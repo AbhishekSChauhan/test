6808f0a5e2989a05e35dc66c44ee27da423bc986
Abhishek Singh <abhishek.uietpuchd@gmail.com>
2021-07-04 17:36:36 +0530

    Trasitions

--- src/HeaderTwo.js
+++ src/HeaderTwo.js
@@ -15,28 +15,28 @@ function HeaderTwo() {
 
                     <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex-shrink-0 flex">
-                            <span className="font-bold font-serif ">Gameonics</span>
+                            <span className="font-bold font-serif text-gray-700 ">Gameonics</span>
                         </div>
 
                         <div className="absolute inset-x-0 left-0 inline-block items-center sm:hidden pl-2">
                             <AiOutlineMenuUnfold />
                         </div>
 
-                        <div className="hidden sm:ml-10 md:ml-28 lg:ml-72 sm:block lg:justify-center"> 
-                            <div className="flex flex-row sm:space-x-11">
-                                <a className="flex flex-row items-center ">                    
+                        <div className="hidden sm:ml-16 md:ml-32 lg:ml-72 sm:block lg:justify-center"> 
+                            <div className="flex flex-row sm:space-x-11 text-gray-600">
+                                <a className="flex flex-row items-center transition duration-150 ease-in-out transform hover:text-gray-800 hover:-translate-y-1 hover:scale-110 ">                    
                                     <ImHome className="mx-0.5"/>
                                     <span >Home</span>
                                 </a>   
-                                <a className="flex flex-row items-center">
+                                <a className="flex flex-row items-center transition duration-150 ease-in-out transform hover:text-gray-800 hover:-translate-y-1 hover:scale-110">
                                     <IoNewspaperSharp className="mx-0.5" />
                                     <span >News</span>
                                 </a>  
-                                <a className="flex flex-row items-center ">
+                                <a className="flex flex-row items-center transition duration-150 ease-in-out transform hover:text-gray-800 hover:-translate-y-1 hover:scale-110">
                                     <FaBlog className="mx-0.5" />
                                     <span >Blog</span>
                                 </a>  
-                                <a className="flex flex-row items-center">
+                                <a className="flex flex-row items-center transition duration-150 ease-in-out transform hover:text-gray-800 hover:-translate-y-1 hover:scale-110">
                                     <FaStarHalfAlt className="mx-0.5" />
                                     <span >Reviews</span>
                                 </a>               
@@ -45,7 +45,7 @@ function HeaderTwo() {
                     </div>
 
                         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 ">
-                                <a className="flex flex-row items-center ">
+                                <a className="flex flex-row items-center text-gray-600 transition duration-150 ease-in-out transform hover:text-gray-800 hover:-translate-y-1 hover:scale-110">
                                         <IoMdLogIn className="mx-1" />
                                         <span className="pt-0.5">LOGIN</span>
                                 </a>
